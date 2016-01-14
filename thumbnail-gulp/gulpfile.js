4a878b0cbcef71167b6970c991b156b6445ec44f
kent10ou <kent10ou@gmail.com>
2016-01-13 22:10:18 -0800

    edit gulpfile

--- thumbnail-gulp/gulpfile.js
+++ thumbnail-gulp/gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp'); // responsible for build process
 var gutil = require('gulp-util'); // responsible for console.log output of build process, for debugging
-var source = require('vinyl-source-stream'); 
+var source = require('vinyl-source-stream'); // vinyl-source-stream convert the readable stream you get from browserify into a vinyl stream that is what gulp is expecting to get.
 var browserify = require('browserify'); // responsible for figuring out one part of code depends on another, make sure load order is correct
 var watchify = require('watchify'); // automatically rerun gulp file when code changes
 var reactify = require('reactify'); // runs in conjunction with browserify to convert jsx to js
@@ -16,4 +16,15 @@ gulp.task('default', function () {
 		packageCache: {},
 		fullPaths: true
 	}))
-})
+
+	function build (file) {
+		if (file) gutil.log('Recompiling' + file);
+		return bundler
+			.bundle()
+			.on('error', gutil.log.bind(gutil, 'Browserify Error'))
+			.pipe(source('main.js'))
+			.pipe(gulp.dest('./'));
+	};
+	build();
+	bundler.on('update', build);
+});
