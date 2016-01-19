ca4698f56ad4613c8f2fa9f1c6eee93f0a7c7687
kent10ou <kent10ou@gmail.com>
2016-01-18 23:22:31 -0800

    update app.js

--- dropdown/src/app.jsx
+++ dropdown/src/app.jsx
@@ -1,19 +1,14 @@
-var options = {
+var React = require('react');
+var Dropdown = require('./dropdown');
 
-  thumbnailData: [{
-    title: 'See Tutorials',
-    number: 32,
-    header: 'Learn React',
-    description: 'React is fantastic new library for making fast, dynamic pages. React is fantastic new library for making fast, dynamic pages.',
-    imageUrl: 'http://formatjs.io/img/react.svg'
-  },{
-    title: 'See Tutorials',
-    number: 25,
-    header: 'Learn Gulp',
-    description: 'Gulp will speed up your development workflow. Gulp will speed up your development workflow.',
-    imageUrl: 'http://brunch.io/images/others/gulp.png'
-  }]
+var options = {
+  title: 'Choose a dessert', // What should show up on the button to open/close the dropdown
+  items: [ // List of items to show in the dropdown
+    'Apple Pie',
+    'Peach Cobbler',
+    'Coconut Cream Pie'
+  ]
 };
 
-var element = React.createElement(ThumbnailList, options);
-ReactDOM.render(element, document.querySelector(".target"));
+var element = React.createElement(Dropdown, options);
+React.render(element, document.querySelector('.container'));
