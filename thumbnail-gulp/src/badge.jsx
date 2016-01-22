bae3383fde7ebed2345e5f35eb3145e0ab398648
kent10ou <kent10ou@gmail.com>
2016-01-22 02:23:07 -0800

    added react and export badge module

--- thumbnail-gulp/src/badge.jsx
+++ thumbnail-gulp/src/badge.jsx
@@ -1,9 +1,10 @@
 var React = require('react');
 
-var Badge = React.createClass({
+module.exports = React.createClass({
 	render: function () {
     return <button className="btn btn-primary" type="button">
     	{this.props.title} <span className="badge">{this.props.number}</span>
   	</button>
 		}
 });
+
